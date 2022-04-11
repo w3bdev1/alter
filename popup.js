8cf81115c8a9783015f193d4cb2d517af02afceb
w3bdev <w3bdev@protonmail.com>
2022-04-11 17:03:57 +0530

    Custom domain feature added

--- popup.js
+++ popup.js
@@ -1,18 +1,88 @@
-const nitterEl = document.getElementById("nitter-instances");
-const tedditEl = document.getElementById("teddit-instances");
-const invidiousEl = document.getElementById("invidious-instances");
-const scribeEl = document.getElementById("scribe-instances");
-const bibliogramEl = document.getElementById("bibliogram-instances");
-const disable = document.getElementById("disable");
+const data = {
+  nitter: {
+    el: document.getElementById("nitter-instances"),
+    selected: "",
+    isCustom: true,
+  },
+  teddit: {
+    el: document.getElementById("teddit-instances"),
+    selected: "",
+    isCustom: true,
+  },
+  invidious: {
+    el: document.getElementById("invidious-instances"),
+    selected: "",
+    isCustom: true,
+  },
+  scribe: {
+    el: document.getElementById("scribe-instances"),
+    selected: "",
+    isCustom: true,
+  },
+  bibliogram: {
+    el: document.getElementById("bibliogram-instances"),
+    selected: "",
+    isCustom: true,
+  },
+  disable: {
+    el: document.getElementById("disable"),
+    selected: "",
+    isCustom: false,
+  },
+};
 
-function createOption(value, isSelected = false) {
+function addEventHandlers() {
+  document.onclick = (e) => {
+    if (e.target.nodeName === "OPTION") {
+      const customInput = e.target.parentNode.nextElementSibling;
+      const addCustomBtn = customInput.nextElementSibling;
+      if (e.target.value === "other") {
+        addCustomBtn.style.display = "none";
+        customInput.style.display = "block";
+        customInput.required = true;
+        return;
+      } else {
+        addCustomBtn.style.display = "inline";
+        customInput.style.display = "none";
+        customInput.required = false;
+      }
+    }
+
+    if (e.target.nodeName === "BUTTON") {
+      if (e.target.className === "add-custom") {
+        const customInput = e.target.previousElementSibling;
+        const selectMenu = customInput.previousElementSibling;
+        e.target.style.display = "none";
+        selectMenu.value = "other";
+        customInput.style.display = "block";
+        customInput.required = true;
+      }
+    }
+  };
+}
+
+function createOption(value, isSelected = false, key) {
   let optionCreated = document.createElement("option");
-  optionCreated.setAttribute("value", value);
+  optionCreated.setAttribute("value", value.toLowerCase());
   if (isSelected === true) {
     optionCreated.setAttribute("selected", "selected");
+    data[key].isCustom = false;
   }
   optionCreated.innerText = value;
   return optionCreated;
+}
+
+function setCustomOptions() {
+  for (let [k, v] of Object.entries(data)) {
+    if (v.isCustom) {
+      const customInput = v.el.nextElementSibling;
+      const addCustomBtn = customInput.nextElementSibling;
+      v.el.value = "other";
+      addCustomBtn.style.display = "none";
+      customInput.style.display = "block";
+      customInput.value = v.selected;
+    }
+  }
 }
 
 browser.runtime.sendMessage({ type: "bg_get_instances" }).then(
@@ -20,46 +90,46 @@ browser.runtime.sendMessage({ type: "bg_get_instances" }).then(
     const instances = msgFromBg.allInstances;
     const currentInstances = msgFromBg.currentInstances;
 
-    instances.nitter.forEach((ins) => {
-      const option = createOption(ins, currentInstances.nitter === ins);
-      nitterEl.appendChild(option);
-    });
+    for (let key of Object.keys(data)) {
+      if (key === "disable") {
+        data.disable.el.checked = currentInstances.disable;
+      } else {
+        instances[key].forEach((ins) => {
+          const option = createOption(ins, currentInstances[key] === ins, key);
+          data[key].el.appendChild(option);
+          data[key].selected = currentInstances[key];
+        });
+        data[key].el.appendChild(createOption("Other"));
+      }
+    }
 
-    instances.teddit.forEach((ins) => {
-      const option = createOption(ins, currentInstances.teddit === ins);
-      tedditEl.appendChild(option);
-    });
-
-    instances.invidious.forEach((ins) => {
-      const option = createOption(ins, currentInstances.invidious === ins);
-      invidiousEl.appendChild(option);
-    });
-
-    instances.scribe.forEach((ins) => {
-      const option = createOption(ins, currentInstances.scribe === ins);
-      scribeEl.appendChild(option);
-    });
-
-    instances.bibliogram.forEach((ins) => {
-      const option = createOption(ins, currentInstances.bibliogram === ins);
-      bibliogramEl.appendChild(option);
-    });
-    disable.checked = currentInstances.disable;
+    setCustomOptions();
+    addEventHandlers();
   },
   (error) => {
     console.log(error);
   }
 );
 
+function getInputValue(element) {
+  if (element.value === "other") {
+    return element.nextElementSibling.value
+      .replace(/^https?:\/\//, "")
+      .replace(/\/$/, "");
+  } else {
+    return element.value;
+  }
+}
+
 document.forms[0].onsubmit = (e) => {
   e.preventDefault();
   const instancesSelected = {
-    nitter: nitterEl.value,
-    teddit: tedditEl.value,
-    invidious: invidiousEl.value,
-    scribe: scribeEl.value,
-    bibliogram: bibliogramEl.value,
-    disable: disable.checked,
+    nitter: getInputValue(data.nitter.el),
+    teddit: getInputValue(data.teddit.el),
+    invidious: getInputValue(data.invidious.el),
+    scribe: getInputValue(data.scribe.el),
+    bibliogram: getInputValue(data.bibliogram.el),
+    disable: data.disable.el.checked,
   };
 
   const sending = browser.runtime.sendMessage({

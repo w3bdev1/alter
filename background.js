0c2be9714b194415fa2268cafc57a6e06fd997fa
tomit4 <mosssap@gmail.com>
2022-12-04 15:54:10 -0800

    Added other quetre instances(commented out)

--- background.js
+++ background.js
@@ -19,6 +19,7 @@ const allInstances = {
   invidious: ["invidious.snopyta.org", "yewtu.be"],
   scribe: ["scribe.rip", "scribe.nixnet.services", "scribe.citizen4.eu"],
   quetre: ["quetre.iket.me", "quetre.vern.cc", "quetre.pussthecat.org"],
+  // quetre: ["quetre.odyssey346.dev", quetre.tokhmi.xyz, quetre.privacydev.net],
 };
 
 let allInstancesArray = [];
